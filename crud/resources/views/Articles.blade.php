9b32b15ee4a54d13841b2c4f6f9e3ebcad250f3a
mojahed <doyourbest123>
2019-03-26 14:59:00 -0600

    model and controller

---
+++ crud/resources/views/Articles.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('public\dist\css\bootstrap.css\bootstrap.min.css')}}">
+    <title>Articles</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>content</th>
+        </tr>
+        @foreach($articles as $article)
+            <tr>
+                <td>{{$article->id}}</td>
+                <td>{{$article->title}}</td>
+                <td>{{$article->content}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
